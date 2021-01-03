ee216864fd28b1ac51033cf66dda7cd78fe0e4af
idoattaly <ido@apiiro.com>
2021-01-03 18:42:28 +0200

    Remove internal from JiraUser setters and add EmailAddress property for api v3

--- Atlassian.Jira/JiraUser.cs
+++ Atlassian.Jira/JiraUser.cs
@@ -8,47 +8,52 @@ namespace Atlassian.Jira
         /// <summary>
         /// The Atlassian account identifier for this user.
         /// </summary>
-        public string AccountId { get; internal set; }
+        public string AccountId { get; set; }
 
         /// <summary>
         /// The identifier for the user as defined by JIRA.
         /// </summary>
-        public string Key { get; internal set; }
+        public string Key { get; set; }
 
         /// <summary>
         /// The 'username' for the user.
         /// </summary>
-        public string Username { get; internal set; }
+        public string Username { get; set; }
 
         /// <summary>
         /// The long display name for the user.
         /// </summary>
-        public string DisplayName { get; internal set; }
+        public string DisplayName { get; set; }
 
         /// <summary>
         /// The email address of the user.
         /// </summary>
-        public string Email { get; internal set; }
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The email address of the user - api v3
+        /// </summary>
+        public string EmailAddress { get; set; }
 
         /// <summary>
         /// Whether the user is marked as active on the server.
         /// </summary>
-        public bool IsActive { get; internal set; }
+        public bool IsActive { get; set; }
 
         /// <summary>
         /// The locale of the User.
         /// </summary>
-        public string Locale { get; internal set; }
+        public string Locale { get; set; }
 
         /// <summary>
         /// Url to access this resource.
         /// </summary>
-        public string Self { get; internal set; }
+        public string Self { get; set; }
 
         /// <summary>
         /// The list of the Avatar URL's for this user
         /// </summary>
-        public AvatarUrls AvatarUrls { get; internal set; }
+        public AvatarUrls AvatarUrls { get; set; }
 
         internal string InternalIdentifier { get; set; }
 
